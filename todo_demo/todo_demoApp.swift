d5d5f9f8bb710fbeef6a07b6ee9b28c2d3884a08
Abdulla Al Mued Ios <mued@alphabytetech.com>
2025-04-13 04:09:58 +0100

    Initial Commit

---
+++ todo_demo/todo_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_demoApp.swift
+//  todo_demo
+//
+//  Created by MA Kader on 13/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct todo_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todo_demoTests/todo_demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  todo_demoTests.swift
+//  todo_demoTests
+//
+//  Created by MA Kader on 13/04/2025.
+//
+
+import Testing
+@testable import todo_demo
+
+struct todo_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
